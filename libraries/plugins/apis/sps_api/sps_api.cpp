7f10fa3b3211b9751f4c6a79b290098306b1e1df
mtrela <mtrela@syncad.com>
2019-03-14 11:09:09 +0100

    A call `database::process_proposals` moved in different place + small refactoring

--- libraries/plugins/apis/sps_api/sps_api.cpp
+++ libraries/plugins/apis/sps_api/sps_api.cpp
@@ -212,7 +212,7 @@ DEFINE_API_IMPL(sps_api_impl, list_voter_proposals) {
   while( result.size() < args.limit && itr != end )
   {
     auto po = _db.find<steem::chain::proposal_object, steem::chain::by_id>(itr->proposal_id);
-    FC_ASSERT(po != nullptr, "Proposal with given id does not exists");
+    FC_ASSERT(po != nullptr, "Proposal with given id does not exist");
     auto apo = api_proposal_object(*po);
     if (args.status == proposal_status::all || apo.is_active(_db.head_block_time()) == args.status)
     {
