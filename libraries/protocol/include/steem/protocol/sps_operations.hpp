951e34b57d15a836ff4eae48de3cc53ac18e346b
mtrela <mtrela@syncad.com>
2019-04-25 13:29:52 +0200

    Small fixes of problems pointed by reviewer

--- libraries/protocol/include/steem/protocol/sps_operations.hpp
+++ libraries/protocol/include/steem/protocol/sps_operations.hpp
@@ -63,17 +63,13 @@ struct remove_proposal_operation : public base_operation
 struct proposal_pay_operation : public virtual_operation
 {
    proposal_pay_operation() = default;
-   proposal_pay_operation(const account_name_type r, const asset& p, transaction_id_type _trx_id, uint16_t _op_in_trx )
-                     : receiver(r), payment(p), trx_id(_trx_id), op_in_trx(_op_in_trx) {}
+   proposal_pay_operation(const account_name_type r, const asset& p )
+                     : receiver(r), payment(p) {}
 
    /// Name of the account which is paid for
    account_name_type receiver;
    /// Amount of SBDs paid.
    asset             payment;
-
-   /// Transaction id + position of operation where appeared a proposal being a source of given operation.
-   transaction_id_type trx_id;
-   uint16_t op_in_trx = 0;
 };
 
 } } // steem::protocol
@@ -125,5 +121,5 @@ FC_REFLECT( steem::protocol::create_proposal_operation, (creator)(receiver)(star
 FC_REFLECT( steem::protocol::update_proposal_votes_operation, (voter)(proposal_ids)(approve) )
 FC_REFLECT( steem::protocol::remove_proposal_operation, (proposal_owner)(proposal_ids) )
 
-FC_REFLECT(steem::protocol::proposal_pay_operation, (receiver)(payment)(trx_id)(op_in_trx))
+FC_REFLECT(steem::protocol::proposal_pay_operation, (receiver)(payment))
 
